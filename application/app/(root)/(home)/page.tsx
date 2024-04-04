fbe84e24ab46dde8de8fcc886308879649f0a8a3
phung hoang <lnphunghoang190903@gmail.com>
2024-04-04 16:38:25 +0700

    Home Page

--- application/app/(root)/(home)/page.tsx
+++ application/app/(root)/(home)/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
-      <h1 className='text-3xl font-bold'>
-        Home
-      </h1>
+      <div className='h-[303px] w-full rounded-[20px] bg-hero bg-cover'>
+
+      </div>
 
     </section>
   )
