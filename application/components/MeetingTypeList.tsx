68ad7ca72475f10c2237969e690f539183b74538
phung hoang <lnphunghoang190903@gmail.com>
2024-04-06 21:27:38 +0700

    Upload files

---
+++ application/components/MeetingTypeList.tsx
@@ -0,0 +1,10 @@
+"use Client"
+
+const MeetingTypeList = () => {
+  return (
+    <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
+    </section>
+  )
+}
+
+export default MeetingTypeList
